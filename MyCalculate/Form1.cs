d933804c004c408116fd3b9fd811d6cde339e95b
N1ikHard <N1ik@yandex.ru>
2021-03-22 15:44:06 +0300

    Добавьте файлы проекта.

---
+++ MyCalculate/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyCalculate
+{
+    public partial class Form1 : Form
+    {
+        int result=0;
+        int number;
+        byte[] array = new byte[0];
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
